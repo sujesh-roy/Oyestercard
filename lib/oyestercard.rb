4278654229920747b80194494716043bce52bb2f
sujesh-roy <52935815+sujesh-roy@users.noreply.github.com>
2019-11-07 12:21:13 +0000

    Updates balance topup

--- lib/oyestercard.rb
+++ lib/oyestercard.rb
@@ -1,2 +1,16 @@
 class Oyestercard
-end
+
+Limit = 90
+    attr_reader :balance
+
+    def initialize()
+        @balance = 0
+    end
+    def topup(amount)
+      if @balance + amount > Limit
+        raise "Please limit your amount to 90"
+      else
+          @balance += amount
+      end
+    end
+  end
--- spec/oyestercard_spec.rb
+++ spec/oyestercard_spec.rb
@@ -1,9 +1,20 @@
 require './lib/oyestercard'
 
 describe Oyestercard do
+    let (:oyestercard) { Oyestercard.new }
+
     it 'creates an instance of Oyestercard' do
-        oyestercard = Oyestercard.new
         expect(oyestercard).to be_instance_of(Oyestercard)
         end
-    
+    it 'Checks if balance is 0' do
+        expect(oyestercard.balance).to eq 0
+        end
+    it 'Adds to the balance' do
+        expect(oyestercard).to respond_to(:topup)
+        end
+    it 'Limits the topup value to be a maximum of 90' do
+        limit = Oyestercard::Limit
+        subject.topup(limit)
+        expect{ subject.topup 1 }.to raise_error "Please limit your amount to 90"
+        end
 end
